a4fdabf157b3a93abae1fd7f5699dfd7950bb70b
vellaG <jeremiahsuarez7267@gmail.com>
2022-11-27 22:21:03 -0600

    i have no idea

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require("path")
+
+//variable hostname, port, rcon port, rcon password
+var hostname;
+var port;
+var rconport;
+var rconpass;
+
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     autoHideMenuBar: true,
-    resizable: false
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname,'preload.js')
+    }
   })
-
   win.loadFile('./page/settings.html')
- 
 }
 
 app.whenReady().then(() => {
   createWindow()
+ 
 })
